4f9769c7a354dfc6be07dbca082b19d4c5001654
Alexa Whitney <alexa.whitney@shopify.com>
2023-04-25 14:58:27 -0600

    added github and fb auth - not working - error says API key not valid

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import base,  { firebaseApp } from '../base';
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -10,7 +13,21 @@ class Inventory extends React.Component {
     deleteFish: PropTypes.func,
     loadSampleFishes: PropTypes.func,
   };
+
+  authHandler = async (authData) => {
+    console.log(authData);
+  };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
   render() {
+    return <Login authenticate={this.authenticate}/>;
     return (
       <div className="inventory">
         <h2>Inventory</h2>
