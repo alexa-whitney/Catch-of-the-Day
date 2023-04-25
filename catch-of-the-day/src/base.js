646e6034664f6febb2a55c50a8a57373568e8af4
Alexa Whitney <alexa.whitney@shopify.com>
2023-04-25 16:45:02 -0600

    Completed Tutorial

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,9 +1,8 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
-require('dotenv').config()
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDiTpcY2FiLVFXuOZGXVU_Sk2SFCHO1nTc",
   authDomain: "catch-of-the-day-alexa-whitney.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-alexa-whitney-default-rtdb.firebaseio.com",
   projectId: "catch-of-the-day-alexa-whitney",
